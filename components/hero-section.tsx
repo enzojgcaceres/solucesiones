e757098ac5066d36347178d6efda5a2367035617
enzojgcaceres <enzojgcaceres@gmail.com>
2025-05-08 23:51:05 -0300

    cambios astrologia demo

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -68,7 +68,7 @@ export default function HeroSection() {
                             }}
                             className="absolute inset-0 -z-20">
                             <Image
-                                src="/mason.jpg"
+                                src="/cielo.jpg"
                                 alt="background"
                                 className="absolute inset-x-0 top-56 -z-20 lg:top-32 dark:block"
                                 width="3276"
@@ -82,7 +82,7 @@ export default function HeroSection() {
                                     <Link
                                         href="#link"
                                         className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950">
-                                        <span className="text-foreground text-sm">Introducing Support for AI Models</span>
+                                        <span className="text-foreground text-sm">Haceme tu consulta</span>
                                         <span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700"></span>
 
                                         <div className="bg-background group-hover:bg-muted size-6 overflow-hidden rounded-full duration-500">
@@ -103,7 +103,7 @@ export default function HeroSection() {
                                     speedSegment={0.3}
                                     as="h1"
                                     className="mt-8 text-balance text-6xl md:text-7xl lg:mt-16 xl:text-[5.25rem]">
-                                    Soluciones para emprender tu sucesión
+                                    Portal de Selva - Astrología
                                 </TextEffect>
                                 <TextEffect
                                     per="line"
@@ -112,7 +112,7 @@ export default function HeroSection() {
                                     delay={0.5}
                                     as="p"
                                     className="mx-auto mt-8 max-w-2xl text-balance text-lg">
-                                    ¿Necesitas iniciar una sucesión y no sabes por donde empezar? Nosotros podemos ayudarte.
+                                    ¿Necesitas leer tus astros? 
                                 </TextEffect>
 
                                 <AnimatedGroup
@@ -174,18 +174,18 @@ export default function HeroSection() {
                                 <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
                                     <Image
                                         className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block"
-                                        src="/firma_dos.jpg"
+                                        src="/manos_lectura.jpg"
                                         alt="app screen"
                                         width="2700"
                                         height="1440"
                                     />
-                                    <Image
+                                    {/* <Image
                                         className="z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden"
-                                        src="/acuerdo_partes.jpg"
+                                        src="/manos_lectura.jpg"
                                         alt="app screen"
                                         width="2700"
                                         height="1440"
-                                    />
+                                    /> */}
                                 </div>
                             </div>
                         </AnimatedGroup>
@@ -204,78 +204,70 @@ export default function HeroSection() {
                         </div>
                         <div className="group-hover:blur-xs mx-auto mt-12 grid max-w-2xl grid-cols-4 gap-x-12 gap-y-8 transition-all duration-500 group-hover:opacity-50 sm:gap-x-16 sm:gap-y-14">
                             <div className="flex">
-                                <img
+                                {/* <Image
                                     className="mx-auto h-5 w-fit dark:invert"
                                     src="https://html.tailus.io/blocks/customers/nvidia.svg"
                                     alt="Nvidia Logo"
                                     height="20"
-                                    width="auto"
-                                />
+                                /> */}
                             </div>
 
                             <div className="flex">
-                                <img
+                                {/* <Image
                                     className="mx-auto h-4 w-fit dark:invert"
                                     src="https://html.tailus.io/blocks/customers/column.svg"
                                     alt="Column Logo"
                                     height="16"
-                                    width="auto"
-                                />
+                                /> */}
                             </div>
                             <div className="flex">
-                                <img
+                                {/* <Image
                                     className="mx-auto h-4 w-fit dark:invert"
                                     src="https://html.tailus.io/blocks/customers/github.svg"
                                     alt="GitHub Logo"
                                     height="16"
-                                    width="auto"
-                                />
+                                /> */}
                             </div>
                             <div className="flex">
-                                <img
+                                {/* <Image
                                     className="mx-auto h-5 w-fit dark:invert"
                                     src="https://html.tailus.io/blocks/customers/nike.svg"
                                     alt="Nike Logo"
                                     height="20"
-                                    width="auto"
-                                />
+                                /> */}
                             </div>
                             <div className="flex">
-                                <img
+                                {/* <Image
                                     className="mx-auto h-5 w-fit dark:invert"
                                     src="https://html.tailus.io/blocks/customers/lemonsqueezy.svg"
                                     alt="Lemon Squeezy Logo"
                                     height="20"
-                                    width="auto"
-                                />
+                                /> */}
                             </div>
                             <div className="flex">
-                                <img
+                                {/* <Image
                                     className="mx-auto h-4 w-fit dark:invert"
                                     src="https://html.tailus.io/blocks/customers/laravel.svg"
                                     alt="Laravel Logo"
                                     height="16"
-                                    width="auto"
-                                />
+                                /> */}
                             </div>
                             <div className="flex">
-                                <img
+                                {/* <Image
                                     className="mx-auto h-7 w-fit dark:invert"
                                     src="https://html.tailus.io/blocks/customers/lilly.svg"
                                     alt="Lilly Logo"
                                     height="28"
-                                    width="auto"
-                                />
+                                /> */}
                             </div>
 
                             <div className="flex">
-                                <img
+                                {/* <Image
                                     className="mx-auto h-6 w-fit dark:invert"
                                     src="https://html.tailus.io/blocks/customers/openai.svg"
                                     alt="OpenAI Logo"
                                     height="24"
-                                    width="auto"
-                                />
+                                /> */}
                             </div>
                         </div>
                     </div>
--- components/ui/text-effect.tsx
+++ components/ui/text-effect.tsx
@@ -177,7 +177,9 @@ const createVariantsWithTransition = (
 ): Variants => {
   if (!transition) return baseVariants;
 
-  const { exit: _, ...mainTransition } = transition;
+  // const { exit: _, ...mainTransition } = transition;
+
+  const { exit, ...mainTransition } = transition;
 
   return {
     ...baseVariants,
